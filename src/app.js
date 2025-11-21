61e07971086755076f177683568634708f318dd8
Abhay Kumar Pandey <abhaypandey7067@gmail.com>
2025-11-21 18:04:43 +0530

    adding cors

--- src/app.js
+++ src/app.js
@@ -4,8 +4,14 @@ const connectDB = require("./config/database");
 const validator = require("validator");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
 
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
